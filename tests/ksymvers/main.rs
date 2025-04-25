7af394d3ce0192ad872a7939c5a0dd93586e86f6
Petr Pavlu <petr.pavlu@suse.com>
2025-04-25 14:31:12 +0200

    Add integration tests for ksymvers

---
+++ tests/ksymvers/main.rs
@@ -0,0 +1,44 @@
+// Copyright (C) 2025 SUSE LLC <petr.pavlu@suse.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+use std::ffi::OsStr;
+
+#[path = "../common/mod.rs"]
+mod common;
+use common::*;
+
+fn ksymvers_run<I: IntoIterator<Item = S>, S: AsRef<OsStr>>(args: I) -> RunResult {
+    tool_run(env!("CARGO_BIN_EXE_ksymvers"), args)
+}
+
+#[test]
+fn compare_cmd_identical() {
+    // Check that the comparison of two identical symvers files shows no differences.
+    let result = ksymvers_run([
+        "compare",
+        "tests/ksymvers/compare_cmd/a.symvers",
+        "tests/ksymvers/compare_cmd/a.symvers",
+    ]);
+    assert!(result.status.success());
+    assert_eq!(result.stdout, "");
+    assert_eq!(result.stderr, "");
+}
+
+#[test]
+fn compare_cmd_changed() {
+    // Check that the comparison of two different symvers files shows relevant differences and
+    // results in the command exiting with a non-zero status.
+    let result = ksymvers_run([
+        "compare",
+        "tests/ksymvers/compare_cmd/a.symvers",
+        "tests/ksymvers/compare_cmd/b.symvers",
+    ]);
+    assert!(!result.status.success());
+    assert_eq!(
+        result.stdout,
+        concat!(
+            "Export 'foo' changed CRC from '0x12345678' to '0x09abcdef'\n", //
+        )
+    );
+    assert_eq!(result.stderr, "");
+}
